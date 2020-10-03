0ea5e05ec0c5edc4984dd9a548963af0272fdcf7
Egor Kirin <egorcom27@mail.ru>
2020-10-03 11:23:39 +0300

    task1

--- src/ru/vsu/num14/Main.java
+++ src/ru/vsu/num14/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         double speed = readSpeed();
-        outputTiming(speed, fallingStoneTimingCheck(speed));
+        outputTimingStone(speed, calculatingTheTimeTheStoneFalls(speed));
     }
 
-    static double fallingStoneTimingCheck(double speed) {
+    static double calculatingTheTimeTheStoneFalls(double speed) {
         // g - Acceleration of gravity
         double g = 9.81;
         return 2 * speed / g;
@@ -20,7 +20,7 @@ public class Main {
         return scanner.nextInt();
     }
 
-    static void outputTiming(double speed, double time) {
+    static void outputTimingStone(double speed, double time) {
         System.out.printf(
                 "With a speed V = %1$.2f, the stone will fall in t = %2$.2f seconds",
                 speed, time
